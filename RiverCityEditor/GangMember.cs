a37c8d0de75eb2a9ab8ba4bcef0a74de4d788542
Frederick Bell <shortstop4313@gmail.com>
2021-10-10 00:21:30 -0400

    Add project files.

---
+++ RiverCityEditor/GangMember.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiverCityEditor
+{
+    class GangMember
+    {
+        int GangId;
+        string Name;
+        int Stamina;
+        int Punch;
+        int Kick;
+        int Weapon;
+        int Throwing;
+        int Agility;
+        int Defense;
+        int Strength;
+        int Willpower;
+        int Cash;
+    }
+}
---
+++ RiverCityEditor/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+namespace RiverCityEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string hexValues;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string directory = string.Empty;
+            RomFile currentRom = new RomFile();
+
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Filter = "NESrom|*.nes|All|*.*";
+            OFD.FileName = "chat";
+            bool? result = OFD.ShowDialog();
+
+            if (result == true)
+            {
+                directory = OFD.FileName;
+
+
+                if (directory.Length > 8)
+                {
+                    currentRom.romByes = new List<byte>(File.ReadAllBytes(directory));
+                    this.OpenRomButton.Background = Brushes.Green;
+                    this.StatusbarText.Text = "File Loaded.";
+                    hexValues = RomFile.ByteArrayToHexString(currentRom.romByes.ToArray());
+                }
+                else
+                {
+                    currentRom.romByes = new List<byte>();
+                    this.OpenRomButton.Background = Brushes.Green;
+                    this.StatusbarText.Text = "File Failed to Load.";
+                    hexValues = string.Empty;
+                }
+            }
+        }
+    }
+}
